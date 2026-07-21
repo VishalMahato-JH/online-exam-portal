33eee9feee68e549507989747e88450353c03da7
Vishal Kumar Mahato <vishalkumarmahato198@gmail.com>
2026-07-21 10:30:53 +0530

    Fix API URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://online-exam-backend-production-24cd.up.railway.appnpm run dev/api"
+  baseURL: "https://online-exam-backend-production-24cd.up.railway.app/api"
 });
 
 export default api
